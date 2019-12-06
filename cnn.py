468a3133b30696a1f02b57ea32b0232cd4068687
wwanturhug <602141901@qq.com>
2019-12-06 22:10:58 +0800

    cnn识别数字

---
+++ cnn.py
@@ -0,0 +1,68 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader, TensorDataset
+
+import input_data
+
+class Net(nn.Module):
+
+	def __init__(self):
+		super(Net, self).__init__()
+		self.conv1 = nn.Conv2d(1, 6, 3)
+		self.conv2 = nn.Conv2d(6 ,16, 3)
+
+		self.fc1 = nn.Linear(16*6*6, 120)
+		self.fc2 = nn.Linear(120, 84)
+		self.fc3 = nn.Linear(84, 10)
+
+	def forward(self, x):
+		x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+		x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+		x = x.view(-1, self.num_flat_features(x))
+		x = F.relu(self.fc1(x))
+		x = F.relu(self.fc2(x))
+		x = self.fc3(x)
+
+		return x
+
+	def num_flat_features(self, x):
+		size = x.size()[1:] #除了batch_size之外的维度
+		num_features = 1
+		for s in size:
+			num_features *= s
+		return num_features
+
+net = Net()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr = 0.001, momentum = 0.9)
+
+train_data, train_label = input_data.train_data_reader()
+print("Train data shape is ", train_data.shape)
+print("Train label shape is ", train_label.shape)
+train_dataset = TensorDataset(train_data, train_label)
+train_loader = DataLoader(
+						dataset = train_dataset,
+						batch_size = 32,
+						shuffle = True,
+						num_workers = 2)
+
+for epoch in range(2):
+	running_loss = 0.0
+	for i, data in enumerate(train_loader, 0):
+		inputs, labels = data
+		optimizer.zero_grad()
+
+		outputs = net(inputs)
+		loss = criterion(outputs, labels)
+		loss.backward()
+		optimizer.step()
+
+		running_loss += loss.item()
+		if i%2000 == 1999:
+			print('[%d, %5d] loss: %.3f' %
+				(epoch + 1, i + 1, running_loss / 2000))
+			running_loss = 0.0
+print("Finished training.")
